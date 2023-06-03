57241c867b448f90d06283ff4be0c9a853e4faf3
Andrew Swint <andrewswint1976@gmail.com>
2023-06-03 10:36:43 -0700

    first commit

---
+++ src/main/java/com/example/textgame/TextGameApplication.java
@@ -0,0 +1,15 @@
+package com.example.textgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextGameApplication {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
