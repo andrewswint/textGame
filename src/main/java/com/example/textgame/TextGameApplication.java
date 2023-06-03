e59d03b456f1c4ad23271d467e46f76ec94e5184
Andrew Swint <andrewswint1976@gmail.com>
2023-06-03 10:59:30 -0700

    remembering scanner

--- src/main/java/com/example/textgame/TextGameApplication.java
+++ src/main/java/com/example/textgame/TextGameApplication.java
@@ -3,10 +3,20 @@ package com.example.textgame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class TextGameApplication {
 
     public static void main(String[] args) {
+
+        String myWeapon;
+        int myHp;
+
+        System.out.println("What is your name...");
+        Scanner scanner = new Scanner(System.in);
+        String playerName = scanner.nextLine();
+        System.out.println("Nice to meet you " + playerName);
 
 
 
