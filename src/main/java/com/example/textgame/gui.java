6d6baedbe92caa4aee357bb8c5372fd42893daa0
Andrew Swint <andrewswint1976@gmail.com>
2023-06-07 19:24:47 -0700

    added start button

--- src/main/java/com/example/textgame/gui.java
+++ src/main/java/com/example/textgame/gui.java
@@ -7,9 +7,11 @@ public class gui {
 
     JFrame window;
     Container container;
-    JPanel titlePanel;
+    JPanel titlePanel, startButtonPanel;
     JLabel titleLabel;
+    JButton startButton;
     Font titleFont = new Font("Times New Roman", Font.PLAIN, 85);
+    Font buttonFont = new Font("Times New Roman", Font.PLAIN, 35);
 
     public static void main(String[] args) {
         gui gui = new gui();
@@ -28,12 +30,24 @@ public class gui {
 
         titlePanel = new JPanel();
         titlePanel.setBounds(100,100,600,150);
-        titlePanel.setBackground(Color.green);
+        titlePanel.setBackground(Color.black);
         titleLabel = new JLabel("Forest Adventure");
-        titleLabel.setForeground(Color.white);
+        titleLabel.setForeground(Color.green);
         titleLabel.setFont(titleFont);
         titlePanel.add(titleLabel);
+
+        startButtonPanel = new JPanel();
+        startButtonPanel.setBounds(300,400,200,100);
+        startButtonPanel.setBackground(Color.black);
+
+        startButton = new JButton("Begin");
+        startButton.setBackground(Color.black);
+        startButton.setForeground(Color.green);
+        startButton.setFont(buttonFont);
+        startButtonPanel.add(startButton);
+
         container.add(titlePanel);
+        container.add(startButtonPanel);
         container.validate();
 
     }
