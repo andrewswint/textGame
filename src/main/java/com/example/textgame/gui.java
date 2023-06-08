260a6cd5b21b07fc2c8c4fad51a447e30c0d708a
Andrew Swint <andrewswint1976@gmail.com>
2023-06-07 19:15:20 -0700

    added text to my window

--- src/main/java/com/example/textgame/gui.java
+++ src/main/java/com/example/textgame/gui.java
@@ -5,7 +5,11 @@ import java.awt.*;
 
 public class gui {
 
-    JFrame window = new JFrame();
+    JFrame window;
+    Container container;
+    JPanel titlePanel;
+    JLabel titleLabel;
+    Font titleFont = new Font("Times New Roman", Font.PLAIN, 85);
 
     public static void main(String[] args) {
         gui gui = new gui();
@@ -14,12 +18,23 @@ public class gui {
 
     public void game() {
 
+        window = new JFrame();
         window.setSize(800,600);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.getContentPane().setBackground(Color.black);
         window.setLayout(null);
         window.setVisible(true);
+        container = window.getContentPane();
 
+        titlePanel = new JPanel();
+        titlePanel.setBounds(100,100,600,150);
+        titlePanel.setBackground(Color.green);
+        titleLabel = new JLabel("Forest Adventure");
+        titleLabel.setForeground(Color.white);
+        titleLabel.setFont(titleFont);
+        titlePanel.add(titleLabel);
+        container.add(titlePanel);
+        container.validate();
 
     }
 }
